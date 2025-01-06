7b357b75dbb3e78fdc9b2a974f9a08784ff09733
Rahul Grover <groverrahul47@gmail.com>
2025-01-06 17:38:07 +0530

    Test problem 4 added

---
+++ test/testProblem4.js
@@ -0,0 +1,7 @@
+/* ==== Problem #4 ====
+The accounting team needs all the years from every car on the lot. Execute a function that will return an array from the dealer data containing only the car years and log the result in the console as it was returned.*/
+
+const inventory = require("../../inventory.js");
+const carYear = require("../problem4.js");
+
+console.log(carYear(inventory));
