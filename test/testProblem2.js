ca76580c626f3b3e9c076591429a1161e09a1360
Rahul Grover <groverrahul47@gmail.com>
2025-01-06 17:37:57 +0530

    Test problem 2 added

---
+++ test/testProblem2.js
@@ -0,0 +1,7 @@
+/*The dealer needs the information on the last car in their inventory. Execute a function to find what the make and model of the last car in the inventory is?  Log the make and model into the console in the format of:
+"Last car is a *car make goes here* *car model goes here*"*/
+
+const inventory = require("../../inventory.js");
+const lastCar = require("../problem2.js");
+
+console.log(lastCar(inventory));
